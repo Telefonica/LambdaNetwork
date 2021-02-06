4afa83966a2b41c29ce3694898c69daafd36ee7e
Biel Tura <bieltv.3@gmail.com>
2021-02-06 11:32:50 +0100

    Compute the FLOPS with audio 1D and its MEL 2D

--- _debug_custom_lambda.py
+++ _debug_custom_lambda.py
@@ -1,14 +1,26 @@
 import torch
 from models.LambdaLayers import LambdaLayer1D
 from models.LambdaLayers import LambdaLayer2D
+from torchscan import summary
+from utils.common_config import get_val_transformations
+from utils.config import create_config
+import torchaudio
 
 def main():
-    # Batch size, number of inputs, dimension of the input
-    audio_input_1d = torch.randn(200, 8, 300)
+
+    config_exp = './configs/lambdaResnet2D_supervised.yml'
+    config_env = './configs/env.yml'
+
+    p = create_config(config_env, config_exp)
+    mel_transform = get_mel_transformation(**p['spectogram_kwargs'])
+
+    # Batch size, number of inputs, dimension of the input (sec x sampling_rate)
+    audio_input_1d = torch.randn(1, 45, 1*16000)
     debug_1d(audio_input_1d)
 
-    img_input_2d = torch.randn(1, 32, 64, 64)
-    debug_2d(img_input_2d)
+    # Batch size, number of inputs, dimension of the input (w x h)
+    audio_input_2d = torch.log(mel_transform(audio_input_1d)+0.001)
+    debug_2d(audio_input_2d)
 
 
 def debug_1d(tensor):
@@ -17,11 +29,17 @@ def debug_1d(tensor):
     
     print('1D Lambda layer (Dense)')
     layer = LambdaLayer1D(d_in, d_out, heads=4, k=16, u=1, m=23, layer_type='dense', dim=dim)
+
+    print('FLOPS for lambda layer 1D (dense):')
+    print(summary(layer, tensor[0].shape))
     print('Input: {}'.format(tensor.shape))
     print('Output: {}'.format(layer(tensor).shape))
 
     print('1D Lambda layer (Conv)')
     layer = LambdaLayer1D(d_in, d_out, heads=4, k=16, u=1, m=23, layer_type='conv')
+
+    print('FLOPS for lambda layer 1D (conv):')
+    print(summary(layer, tensor[0].shape))
     print('Input: {}'.format(tensor.shape))
     print('Output: {}'.format(layer(tensor).shape))
 
@@ -33,8 +51,19 @@ def debug_2d(tensor):
     print('2D Lambda layer (Conv)')
     layer = LambdaLayer2D(in_channels, out_channels, heads=4, k=16, u=1, m=23)
 
+    print('FLOPS for lambda layer 2D:')
+    print(summary(layer, tensor[0].shape))
     print('Input: {}'.format(tensor.shape))
     print('Output: {}'.format(layer(tensor).shape))
+
+
+def get_mel_transformation(sample_rate, n_fft, n_mels, win_size, win_stride):
+    return torchaudio.transforms.MelSpectrogram(
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            n_mels=n_mels,
+            win_length=int(win_size*sample_rate),
+            hop_length=int(win_stride*sample_rate) )
 
 
 if __name__ == "__main__":
