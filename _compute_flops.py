10ec122fa42b6150b07bc8a7f4a208c27ee096f0
Biel Tura <bieltv.3@gmail.com>
2021-02-06 11:02:52 +0100

    Lambda layer 1D convolution

--- _compute_flops.py
+++ _compute_flops.py
@@ -1,4 +1,5 @@
 from models.Resnets import ResNet18, ResNet50
+from models.LightResnets import ResNet15
 from models.LambdaResnets import LambdaResNet18, LambdaResNet50
 from torchscan import summary
 from utils.common_config import get_dataset, get_train_transformations
@@ -12,11 +13,13 @@ p = create_config(config_env, config_exp)
 train_transforms = get_train_transformations(p)
 train_dataset = get_dataset(p, train_transforms, to_augmented_dataset=False, subset="training")
 
-backbone = LambdaResNet18(in_channels=1)
-#backbone = ResNet18(in_channels=1)
+#backbone = LambdaResNet18(in_channels=1)
+backbone = ResNet18(in_channels=1)
 
 #backbone = LambdaResNet50(in_channels=1)
 #backbone = ResNet50(in_channels=1)
+
+#backbone = ResNet15(in_channels=1)
 
 #num_labels = 10
 #num_labels = 20
--- _debug_custom_lambda.py
+++ _debug_custom_lambda.py
@@ -1,33 +1,40 @@
 import torch
-from models.LambdaLayers import LambdaLayer1D_new
 from models.LambdaLayers import LambdaLayer1D
 from models.LambdaLayers import LambdaLayer2D
 
-
 def main():
     # Batch size, number of inputs, dimension of the input
-    #audio_input_1d = torch.randn(200, 5, 300)
     audio_input_1d = torch.randn(200, 8, 300)
     debug_1d(audio_input_1d)
 
-    #img_input_2d = torch.randn(1, 32, 64, 64)
-    #debug_2d(img_input_2d)
+    img_input_2d = torch.randn(1, 32, 64, 64)
+    debug_2d(img_input_2d)
 
 
 def debug_1d(tensor):
+    batch, d_in, dim = tensor.shape
+    d_out = d_in
+    
+    print('1D Lambda layer (Dense)')
+    layer = LambdaLayer1D(d_in, d_out, heads=4, k=16, u=1, m=23, layer_type='dense', dim=dim)
+    print('Input: {}'.format(tensor.shape))
+    print('Output: {}'.format(layer(tensor).shape))
 
-    batch, in_tokens, dim = tensor.shape
-    out_tokens = in_tokens
-    layer = LambdaLayer1D_new(dim, in_tokens, out_tokens, heads=4, k=16, u=1, m=23)
+    print('1D Lambda layer (Conv)')
+    layer = LambdaLayer1D(d_in, d_out, heads=4, k=16, u=1, m=23, layer_type='conv')
+    print('Input: {}'.format(tensor.shape))
+    print('Output: {}'.format(layer(tensor).shape))
 
-    print(layer(tensor).shape)
 
 def debug_2d(tensor):
-    batch, channels, h, w = tensor.shape
+    batch, in_channels, h, w = tensor.shape
     out_channels = in_channels
+    
+    print('2D Lambda layer (Conv)')
     layer = LambdaLayer2D(in_channels, out_channels, heads=4, k=16, u=1, m=23)
 
-    print(layer(tensor).shape)
+    print('Input: {}'.format(tensor.shape))
+    print('Output: {}'.format(layer(tensor).shape))
 
 
 if __name__ == "__main__":
